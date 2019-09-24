01110bd95a3ef0f8f9ecbffe730a5549ac9f30bd
DianRP43 <55683288+DianRP43@users.noreply.github.com>
2019-09-24 15:12:14 +0700

    Add files via upload
    PBO1 - IF-01

---
+++ PBO1-10118043-Latihan6-KambingStaticKonstanta/src/pbo1/pkg10118043/latihan6/kambingstatickonstanta/PBO110118043Latihan6KambingStaticKonstanta.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118043.latihan6.kambingstatickonstanta;
+
+/**
+ *
+ * @author user
+ */
+
+public class PBO110118043Latihan6KambingStaticKonstanta {
+    public static final String nk = "Midun";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mamalia.jk = 485000;
+        System.out.println(nk + " Memiliki kambing sebanyak " + Mamalia.jk);
+    }
+    
+}
